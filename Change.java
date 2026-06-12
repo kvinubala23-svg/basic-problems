5659e0c246eb4b117bc805b2efcb527fb302c02b
Vinu bala k <kvinubala23@gmail.com>
2026-06-13 00:15:20 +0530

    Add input handling and calculation in Change.java
    Fix calculation logic to properly sum two integers.The output is double variable.

---
+++ Change.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Change{
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int n1=sc.nextInt();
+    double d;
+    d=n+n1;
+    System.out.print(d);
+  }
+}
+    
